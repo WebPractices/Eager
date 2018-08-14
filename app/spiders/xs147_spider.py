7d56480fde63c9e14b854eb48ca56da2aa08f350
yukun <15152347277@163.com>
2018-08-14 22:02:59 +0800

    add 147xs spider

--- app/spiders/xs147_spider.py
+++ app/spiders/xs147_spider.py
@@ -6,20 +6,21 @@ from app.utils.parse_url import parse_url
 from app.database import MongodbClient
 
 
-class CrawlBiquge(object):
+class CrawlXs147(object):
     """
-    Biquge crawler
-    source: http://www.biquge.com.tw/
+    147xs crawler
+    source: http://www.147xs.com/
     """
+
     def __init__(self):
-        self._biquge = 'http://www.biquge.com.tw/{category}/'
-        self._base_url = 'http://www.biquge.com.tw'
+        self._base_url = 'http://www.147xs.com'
+        self._xs147 = 'http://www.147xs.com/sort/{category}/'
         self._novel_html = None
         self.crawl_time = datetime.now()
 
     def _novel_state(self, update_info):
         """get novels state, judge this novel is finished or unfinished"""
-        spt = list(map(int, update_info[5:].split('-')))
+        spt = list(map(int, update_info.split('-')))
         update_time = datetime(spt[0], spt[1], spt[2])
         interval_days = (self.crawl_time - update_time).days
         state = '更新中' if interval_days < 7 else '完结'
@@ -27,35 +28,30 @@ class CrawlBiquge(object):
 
     def get_url(self, category):
         """crawler all novels url according to category"""
-        url = self._biquge.format(category=category)
-        res = parse_url(url, 'gbk')
+        url = self._xs147.format(category=category)
+        res = parse_url(url, 'utf-8')
         html = etree.HTML(res)
-        urls = html.xpath('//*[@id="newscontent"]/div[@class="l"]/ul/li/span[1]/a/@href')
-        urls_r = html.xpath('//*[@id="newscontent"]/div[@class="r"]/ul/li/span[1]/a/@href')
-        names = html.xpath('//*[@id="newscontent"]/div[@class="l"]/ul/li/span[1]/a/text()')
-        names_r = html.xpath('//*[@id="newscontent"]/div[@class="r"]/ul/li/span[1]/a/text()')
-        authors = html.xpath('//*[@id="newscontent"]/div[@class="l"]/ul/li/span[3]/text()')
-        authors_r = html.xpath('//*[@id="newscontent"]/div[@class="r"]/ul/li/span[2]/text()')
-        urls.extend(urls_r)
-        names.extend(names_r)
-        authors.extend(authors_r)
+        urls = html.xpath('//*[@id="main"]/div[@class="novelslist"]/div[1]/ul/li/a/@href')
+        names = html.xpath('//*[@id="main"]/div[@class="novelslist"]/div[1]/ul/li/a/text()')
+        authors = html.xpath('//*[@id="main"]/div[@class="novelslist"]/div[1]/ul/li/text()')
         for url, name, author in zip(urls, names, authors):
             novel = {
                 'source_url': url,
                 'name': name,
-                'author': author
+                'author': author.lstrip('/')
             }
             yield novel
 
     def get_info(self, novel_url):
         """crawler novel info"""
-        res = parse_url(novel_url, 'gbk')
+        res = parse_url(novel_url, 'utf-8')
         html = etree.HTML(res)
         self._novel_html = html
-        image = html.xpath('//*[@id="fmimg"]/img/@src')[0]
-        intro = html.xpath('//*[@id="intro"]/p/text()')[0]
+        image = html.xpath('//*[@id="fmimg"]/a/img/@src')[0]
+        intro = html.xpath('//*[@id="intro"]/text()')[2]
         update_info = html.xpath('//*[@id="info"]/p[3]/text()')[0]
-        state = self._novel_state(update_info)
+        _datetime = update_info.split('：')[1].split(' ')[0]
+        state = self._novel_state(_datetime)
         info = {
             'image': self._base_url + image,
             'intro': intro,
@@ -65,13 +61,13 @@ class CrawlBiquge(object):
 
     def get_menu(self, url=None):
         """crawling novel menu"""
-        if self._novel_html is not None:
+        if self._novel_html is None:
             if url:
-                res = parse_url(url, 'gbk')
+                res = parse_url(url, 'utf-8')
                 self._novel_html = etree.HTML(res)
             else: return False
-        chapter_url = self._novel_html.xpath('//*[@id="list"]/dl/dd/a/@href')
-        chapter_title = self._novel_html.xpath('//*[@id="list"]/dl/dd/a/text()')
+        chapter_url = self._novel_html.xpath('//*[@id="list"]/dd/a/@href')
+        chapter_title = self._novel_html.xpath('//*[@id="list"]/dd/a/text()')
         for url, title in zip(chapter_url, chapter_title):
             chapter = {
                 'source_url': self._base_url + url,
@@ -81,26 +77,39 @@ class CrawlBiquge(object):
 
     def get_chapter(self, chapter_url):
         """crawler chapter content"""
-        res = parse_url(chapter_url, 'gbk')
+        res = parse_url(chapter_url, 'utf-8')
         html = etree.HTML(res)
-        content = html.xpath('//*[@id="content"]/text()')
-        return ''.join(content)
-
-    def __call__(self, *args, **kwargs):
-        pass
+        content = html.xpath('//*[@id="content"]/p/text()')
+        return '\n'.join(content)
 
 if __name__ == '__main__':
     import time
+
     db = MongodbClient('novel', 'localhost', 27017)
-    crawler = CrawlBiquge()
-    cates = ['xuanhuan', 'xiuzhen', 'dushi', 'lishi', 'wangyou', 'kehuan', 'kongbu']
-    for category in cates:
+    crawler = CrawlXs147()
+    cates = {
+        '1': 'xuanhuan',
+        '2': 'xiuzhen',
+        '3': 'dushi',
+        '4': 'lishi',
+        '5': 'zhichang',
+        '6': 'wangyou',
+        '7': 'kehuan',
+        '8': 'junshi',
+        '9': 'lingyi',
+        '10': 'qihuan',
+        '11': 'wuxia',
+        '12': 'jingji',
+        '13': 'tongren',
+        '14': 'yanqing'
+    }
+    for category, value in cates.items():
         for novel in crawler.get_url(category):
             db.change_table('novel')
-            novel['category'] = category
+            novel['category'] = value
             info = crawler.get_info(novel['source_url'])
             data = dict(novel, **info)
-            print(data)
+            print('success novel: ', data['source_url'])
             db.put(data)
             db.change_table('chapter')
             for c in crawler.get_menu(novel['source_url']):
@@ -109,8 +118,6 @@ if __name__ == '__main__':
                 c['content'] = content
                 c['novel'] = novel['name']
                 c['author'] = novel['author']
-                print(c)
+                print('success chapter: ', c['source_url'])
                 db.put(c)
-
-
 
